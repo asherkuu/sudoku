3be1ef3e48b433261f10cf753c03e22a8bd713df
asherkuu <asher_kuu@naver.com>
2021-11-26 18:36:48 +0900

    style 변경

--- src/components/numbers/index.tsx
+++ src/components/numbers/index.tsx
@@ -1,17 +1,17 @@
-import React, { FC } from 'react'
+import React, { FC } from "react";
 
-import Button from './button'
-import { NUMBERS } from 'typings'
-import { Container } from './styles'
+import Button from "./button";
+import { NUMBERS } from "typings";
+import { Container } from "./styles";
 
 const Numbers: FC = () => {
-   return (
-      <Container>
-         {([1, 2, 3, 4, 5, 6, 7, 8, 9] as NUMBERS[]).map(value => (
-            <Button key={value} value={value} />
-         ))}
-      </Container>
-   )
-}
+  return (
+    <Container>
+      {([1, 2, 3, 4, 5, 6, 7, 8, 9] as NUMBERS[]).map((value) => (
+        <Button key={value} value={value} />
+      ))}
+    </Container>
+  );
+};
 
-export default Numbers
+export default Numbers;
--- src/components/styles/card/index.ts
+++ src/components/styles/card/index.ts
@@ -1,12 +1,11 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components";
 
 export const Card = styled.div`
-   ${({ theme }) => css `
-      background-color: ${theme.colors.white};
-      border-radius: 15px;
-      flex: 1;
-      flex-direction: column;
-      max-height: fit-content;
-      padding: 15px;
-   `}
+  ${({ theme }) => css`
+    background-color: ${theme.colors.white};
+    border-radius: 15px;
+    flex-direction: column;
+    max-height: fit-content;
+    padding: 15px;
+  `}
 `;
